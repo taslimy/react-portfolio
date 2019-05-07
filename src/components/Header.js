64c764df20b606c7d49402dfa6d10f747b90d3e7
taslimy <taslimer@gmail.com>
2019-05-07 00:15:02 -0400

    Fixed some styling errors, routes working, pages displaying properly

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,34 +6,35 @@ export default class Header extends Component {
   render() {
     return (
       <Fragment>
-      <header>
-        <h1>
-          <Link to="/">
-            Taslim Yakub
-            <em className="webdev"> &LT; web developer &sol; &GT; </em>
-          </Link>
-        </h1>
-        <nav>
-          <input type="checkbox" id="nav" className="hidden" />
-          <label for="nav" class="nav-open">
-            <i />
-            <i />
-            <i />
-          </label>
-          <div className="nav-container">
-            <ul>
-              <li>
-                <Link to="/">
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link to="/contact">Bio &amp; Contact</Link>
-              </li>
-            </ul>
-          </div>
-        </nav>
-      </header> 
+        <header>
+          <h1>
+            <Link to="/">
+              Taslim Yakub
+              <em className="webdev"> &LT; web developer &sol; &GT; </em>
+            </Link>
+          </h1>
+          <nav>
+            <input type="checkbox" id="nav" className="hidden" />
+            <label for="nav" class="nav-open">
+              <i />
+              <i />
+              <i />
+            </label>
+            <div className="nav-container">
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/">Blog</Link>
+                </li>
+                <li>
+                  <Link to="/contact">Bio &amp; Contact</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </header>
       </Fragment>
     );
   }
